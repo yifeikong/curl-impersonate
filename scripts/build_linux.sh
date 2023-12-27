67502a1559c5c8daa6b9146e30183140052439a8
Brett Jia <dev.bjia56@gmail.com>
2023-12-27 15:45:42 -0500

    use correct golang arch

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -3,7 +3,7 @@
 python3.9 -m pip install ninja cmake
 export PATH=/opt/_internal/cpython-3.9.18/bin:$PATH
 
-curl -o /tmp/go.tar.gz -L https://go.dev/dl/go1.19.10.linux-armv6l.tar.gz
+curl -o /tmp/go.tar.gz -L https://go.dev/dl/go1.19.10.linux-amd64.tar.gz
 tar -C /usr/local -xzf /tmp/go.tar.gz
 export PATH=/usr/local/go/bin:$PATH
 
