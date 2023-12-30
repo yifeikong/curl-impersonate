64707cf6a4e6d7c93a8c0ec1dd39eee256b9f3d8
Yifei Kong <kong@yifei.me>
2023-12-30 23:09:44 +0800

    Build with switch on

--- win/build.sh
+++ win/build.sh
@@ -26,31 +26,32 @@ mv lib/ssl/libssl.a lib/libssl.a
 
 cd ..
 
-export IPV6=1
 export ZLIB=1
 export ZLIB_PATH=zlib_stub
-export ZSTD=1
 export ZSTD_PATH=zstd_stub
-export BROTLI=1
 export BROTLI_PATH=brotli_stub
 export BROTLI_LIBS='-lbrotlidec -lbrotlicommon'
-export NGHTTP2=1
 export NGHTTP2_PATH=nghttp2_stub
-export IDN2=1
 export LIBIDN2_PATH=idn2_stub
 export SSL=1
 export OPENSSL_PATH=$PWD/boringssl
 export OPENSSL_LIBPATH=$PWD/boringssl/lib
 export OPENSSL_LIBS='-lssl -lcrypto'
+
+export IPV6=1
 export HTTP2=1
 export WEBSOCKETS=1
 export ECH=1
+export LIBS='-l'
 
 CURL_VERSION=curl-8_1_1
 
 curl -L https://github.com/curl/curl/archive/${CURL_VERSION}.zip -o curl.zip
 unzip -q -o curl.zip
 mv curl-${CURL_VERSION} curl
+
+# Apparently, building curl on windows has changes since this commit:
+# https://github.com/curl/curl/commit/a8861b6ccdd7ca35b6115588a578e36d765c9e38
 
 cd curl
 
@@ -66,8 +67,8 @@ sed -i 's/-DUSE_NGHTTP2/-DUSE_NGHTTP2 -DNGHTTP2_STATICLIB/g' src/Makefile.mk
 sed -i 's/-lidn2/-lidn2 -lunistring -liconv/g' lib/Makefile.mk
 sed -i 's/-lidn2/-lidn2 -lunistring -liconv/g' src/Makefile.mk
 
-mingw32-make -f Makefile.dist mingw32-clean CFLAGS=-Wno-unused-variable
-mingw32-make -f Makefile.dist mingw32 -j CFLAGS=-Wno-unused-variable
+# mingw32-make -f Makefile.dist mingw32-clean CFLAGS=-Wno-unused-variable
+mingw32-make -f Makefile.dist mingw32 -j CFLAGS=-Wno-unused-variable CFG=-zlib,-nghttp2,-idn2,-brotli,-zstd
 
 mkdir -p ../dist
 mv lib/libcurl* ../dist/
